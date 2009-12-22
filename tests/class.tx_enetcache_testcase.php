45004019b8354d0e4051bb4f8c97db4aa7da1e52
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-22 01:02:21 +0000

    Added two rough and simple speed tests
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/enetcache/trunk@27934 735d13b6-9817-0410-8766-e36946ffe9aa

--- tests/class.tx_enetcache_testcase.php
+++ tests/class.tx_enetcache_testcase.php
@@ -18,18 +18,41 @@ class tx_enetcache_testcase extends tx_phpunit_testcase {
 	 */
 	protected function setUp() {
 		parent::setUp ();
-		$GLOBALS['typo3CacheManager']->setCacheFactory(t3lib_div::makeInstance(t3lib_cache_Factory));
-		$GLOBALS['typo3CacheManager']->setCacheConfigurations(array('cache_enetcache_content'=>$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_enetcache_content']));
-		$GLOBALS['typo3CacheManager']->initialize();
+//		$GLOBALS['typo3CacheManager']->setCacheFactory(t3lib_div::makeInstance('t3lib_cache_Factory'));
+//		$GLOBALS['typo3CacheManager']->setCacheConfigurations(array('cache_enetcache_content'=>$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']));
+		//		$GLOBALS['typo3CacheManager']->initialize();
+		$this->cache = t3lib_div::makeInstance('tx_enetcache');
+		$this->tags = array();
+		for($i = 0; $i<5000; $i++) {
+			$this->tags[] = strval(rand());
+		}
 	}
 
 	/**
 	 * Tests Subpart substitution
 	 */
 	public function testAddingCacheEntry() {
-		tx_enetcache::set(array(1, $this, 'test', array('more', 'test'),1), 'cached data', array('more', 'test'));
-		$this->assertEquals('cached data', tx_enetcache::get(array(1, $this, 'test', array('more', 'test'), 1)));
-		tx_enetcache::drop(array('more', 'test'), 1);
+		$this->cache->set(array(1, $this, 'test', array('more', 'test'),1), 'cached data', array('more', 'test'));
+		
+		$this->assertEquals('cached data', $this->cache->get(array(1, $this, 'test', array('more', 'test'), 1)));
+
+			//Cleanup
+		$this->cache->drop(array('more', 'test'));
+	}
+
+	public function testSetManyTags() {
+		$microtimeBeforeSet = microtime(1);
+		$this->cache->set(array('cachetest'), 'test data', $this->tags);
+		$microtimeAfterSet = microtime(1);
+		$this->assertLessThan(0.0001, $microtimeAfterSet - $microtimeBeforeSet, 'Setting takes too long');
+	}
+
+	public function testDropManyTags() {
+		$microtimeBeforeDrop = microtime(1);
+		$this->cache->drop($this->tags);
+		$microtimeAfterDrop = microtime(1);
+		$this->assertLessThan(0.0001, $microtimeAfterDrop - $microtimeBeforeDrop, 'Dropping takes too long');
+
 	}
 }
 ?>
