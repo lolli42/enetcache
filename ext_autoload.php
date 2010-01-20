3518cc9df210863c5d8c198508c01d930d5ebac5
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-20 21:18:12 +0000

    Bugfix: Moved class.tx_enetcache_tcaHandler.php to lowercase filename
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/enetcache/trunk@29132 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -6,7 +6,7 @@ $extensionPath = t3lib_extMgm::extPath('enetcache');
 return array(
 	'tx_enetcache' => $extensionPath . 'classes/class.tx_enetcache.php',
 	'tx_enetcache_hook' => $extensionPath . 'classes/class.tx_enetcache_hook.php',
-	'tx_enetcache_tcahandler' => $extensionPath . 'classes/class.tx_enetcache_tcaHandler.php',
+	'tx_enetcache_tcahandler' => $extensionPath . 'classes/class.tx_enetcache_tcahandler.php',
 	'tx_enetcache_cache_backend_compresseddbbackend' => $extensionPath . 'classes/class.tx_enetcache_cache_backend_compresseddbbackend.php',
 	'tx_enetcache_backendcontentcacheaction' => $extensionPath . 'hooks/class.tx_enetcache_backendContentCacheAction.php',
 	'tx_enetcache_tcemain' => $extensionPath . 'hooks/class.tx_enetcache_tcemain.php',
