d94cb494b7e06912fbaec9dcf5a6bc4f9c2dde9a
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-23 09:47:18 +0000

    Drop garbage collection task, this is in the core since TYPO3 4.5. Add documentation about migration, delete classes, clean up autoloader and localconf
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/enetcache/trunk@70242 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,13 +3,6 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-	// Register own implementation of a compressed db backend for TYPO3 versions 4.3 and 4.4.
-	// @obsolete since TYPO3 4.5: Use compress option of core database backend instead
-if (t3lib_div::int_from_ver(TYPO3_version) <= '4004999') {
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheBackends']['tx_enetcache_cache_backend_CompressedDbBackend']
-		= 'typo3conf/ext/enetcache/classes/class.tx_enetcache_cache_backend_compresseddbbackend.php:tx_enetcache_cache_backend_CompressedDbBackend';
-}
-
 	// Add a new cache configuration if not already set in localconf.php
 if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache'])) {
 	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache'] = array();
@@ -65,17 +58,6 @@ if (TYPO3_MODE == 'BE') {
 		'EXT:enetcache/hooks/class.tx_enetcache_tcemain.php:tx_enetcache_tcemain';
 	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['enetcache'] =
 		'EXT:enetcache/hooks/class.tx_enetcache_tcemain.php:tx_enetcache_tcemain';
-
-		// Scheduler task for garbage collection of cache backends (especially db- and fileBackend)
-		// @obsolete since TYPO3 4.5, use the core task instead
-	if (t3lib_div::int_from_ver(TYPO3_version) <= '4004999') {
-		$TYPO3_CONF_VARS['SC_OPTIONS']['scheduler']['tasks']['tx_enetcache_gccachebackends'] = array(
-			'extension' => $_EXTKEY,
-			'title' => 'LLL:EXT:enetcache/locallang.xml:scheduler.gccachebackends.name',
-			'description' => 'LLL:EXT:enetcache/locallang.xml:scheduler.gccachebackends.description',
-			'additionalFields' => 'tx_enetcache_gccachebackends_additionalfieldprovider',
-		);
-	}
 
 		// Scheduler task to drop cache entries by tags
 	$TYPO3_CONF_VARS['SC_OPTIONS']['scheduler']['tasks']['tx_enetcache_task_DropTags'] = array(
