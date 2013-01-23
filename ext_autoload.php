d8bf2ae5de0d88ceccaf8f737dd8d1f7da55c78d
lolli <lolli@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-23 15:19:23 +0000

    Move lowercase classes to uppercase Classes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/enetcache/trunk@70263 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,9 +1,9 @@
 <?php
 $extensionPath = t3lib_extMgm::extPath('enetcache');
 return array(
-	'tx_enetcache' => $extensionPath . 'classes/class.tx_enetcache.php',
-	'tx_enetcache_hook' => $extensionPath . 'classes/class.tx_enetcache_hook.php',
-	'tx_enetcache_tcahandler' => $extensionPath . 'classes/class.tx_enetcache_tcahandler.php',
+	'tx_enetcache' => $extensionPath . 'Classes/class.tx_enetcache.php',
+	'tx_enetcache_hook' => $extensionPath . 'Classes/class.tx_enetcache_hook.php',
+	'tx_enetcache_tcahandler' => $extensionPath . 'Classes/class.tx_enetcache_tcahandler.php',
 	'tx_enetcache_backendcontentcacheaction' => $extensionPath . 'hooks/class.tx_enetcache_backendContentCacheAction.php',
 	'tx_enetcache_tcemain' => $extensionPath . 'hooks/class.tx_enetcache_tcemain.php',
 	'tx_enetcache_hookable' => $extensionPath . 'interfaces/interface.tx_enetcache_hookable.php',
@@ -11,11 +11,11 @@ return array(
 	'tx_enetcache_task_droptags' => $extensionPath . 'tasks/class.tx_enetcache_task_droptags.php',
 	'tx_enetcache_task_droptags_additionalfieldprovider' => $extensionPath . 'tasks/class.tx_enetcache_task_droptags_additionalfieldprovider.php',
 	'tx_enetcache_cli' => $extensionPath . 'cli/class.tx_enetcache_cli.php',
-	'tx_enetcache_extensionwrappers_pi_abstract' => $extensionPath . 'classes/extensionwrappers/class.tx_enetcache_extensionwrappers_pi_abstract.php',
-	'tx_enetcache_extensionwrappers_vge_tagcloud_pi1' => $extensionPath . 'classes/extensionwrappers/class.tx_enetcache_extensionwrappers_vge_tagcloud_pi1.php',
-	'tx_enetcache_extensionwrappers_wec_map_pi_abstract' => $extensionPath . 'classes/extensionwrappers/class.tx_enetcache_extensionwrappers_wec_map_pi_abstract.php',
-	'tx_enetcache_extensionwrappers_wec_map_pi1' => $extensionPath . 'classes/extensionwrappers/class.tx_enetcache_extensionwrappers_wec_map_pi1.php',
-	'tx_enetcache_extensionwrappers_wec_map_pi2' => $extensionPath . 'classes/extensionwrappers/class.tx_enetcache_extensionwrappers_wec_map_pi2.php',
-	'tx_enetcache_extensionwrappers_wec_map_pi3' => $extensionPath . 'classes/extensionwrappers/class.tx_enetcache_extensionwrappers_wec_map_pi3.php',
+	'tx_enetcache_extensionwrappers_pi_abstract' => $extensionPath . 'Classes/extensionwrappers/class.tx_enetcache_extensionwrappers_pi_abstract.php',
+	'tx_enetcache_extensionwrappers_vge_tagcloud_pi1' => $extensionPath . 'Classes/extensionwrappers/class.tx_enetcache_extensionwrappers_vge_tagcloud_pi1.php',
+	'tx_enetcache_extensionwrappers_wec_map_pi_abstract' => $extensionPath . 'Classes/extensionwrappers/class.tx_enetcache_extensionwrappers_wec_map_pi_abstract.php',
+	'tx_enetcache_extensionwrappers_wec_map_pi1' => $extensionPath . 'Classes/extensionwrappers/class.tx_enetcache_extensionwrappers_wec_map_pi1.php',
+	'tx_enetcache_extensionwrappers_wec_map_pi2' => $extensionPath . 'Classes/extensionwrappers/class.tx_enetcache_extensionwrappers_wec_map_pi2.php',
+	'tx_enetcache_extensionwrappers_wec_map_pi3' => $extensionPath . 'Classes/extensionwrappers/class.tx_enetcache_extensionwrappers_wec_map_pi3.php',
 );
 ?>
