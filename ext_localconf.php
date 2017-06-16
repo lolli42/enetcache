5a300be90f10b2129b8c04f57f92fb1acd70f47e
Christian Kuhn <lolli@schwarzbu.ch>
2017-06-16 22:59:38 +0200

    [TASK] Cleanup ext_localconf ext_tables

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,73 +3,57 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-	// Add a new cache configuration if not already set in localconf.php
+// Add a new cache configuration if not already set in localconf.php
 if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache'])) {
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache'] = array();
+	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache'] = [];
 }
-	// Use StringFrontend if not set otherwise, if not set, core would choose variable frontend
+// Use StringFrontend if not set otherwise, if not set, core would choose variable frontend
 if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['frontend'])) {
 	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['frontend'] = 't3lib_cache_frontend_StringFrontend';
 }
-	// Add cache settings for core versions below 4.6
-if (Tx_Enetcache_Utility_Compatibility::convertVersionNumberToInteger(TYPO3_version) <= '4005999') {
-	if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['backend'])) {
-		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['backend'] = 't3lib_cache_backend_DbBackend';
-	}
-	if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['options'])) {
-		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['options'] = array();
-	}
-	if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['options']['cacheTable'])) {
-		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['options']['cacheTable'] = 'tx_enetcache_contentcache';
-	}
-	if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['options']['tagsTable'])) {
-		$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_enetcache_contentcache']['options']['tagsTable'] = 'tx_enetcache_contentcache_tags';
-	}
-}
 
-	// Define caches that have to be tagged and dropped
-$TYPO3_CONF_VARS['EXTCONF']['enetcache']['TAG_CACHES'] = array(
+// Define caches that have to be tagged and dropped
+$TYPO3_CONF_VARS['EXTCONF']['enetcache']['TAG_CACHES'] = [
 	'cache_enetcache_contentcache',
 	'cache_pages',
-);
+];
 
-	// Initialize array for hooks. Other extensions can register here (like enetcacheanalytics)
+// Initialize array for hooks. Other extensions can register here
 if (!isset($TYPO3_CONF_VARS['EXTCONF']['enetcache']['hooks']['tx_enetcache'])) {
-	$TYPO3_CONF_VARS['EXTCONF']['enetcache']['hooks']['tx_enetcache'] = array();
+	$TYPO3_CONF_VARS['EXTCONF']['enetcache']['hooks']['tx_enetcache'] = [];
 }
 
-	// Configure BE hooks
+// Configure BE hooks
 if (TYPO3_MODE == 'BE') {
-		// Add the "Delete plugin cache" button and its functionality
+	// Add the "Delete plugin cache" button and its functionality
 	$TYPO3_CONF_VARS['BE']['AJAX']['enetcache::clearContentCache'] =
 		'EXT:enetcache/hooks/class.tx_enetcache_backendContentCacheMethods.php:tx_enetcache_backendContentCacheMethods->clearContentCache';
 	$TYPO3_CONF_VARS['SC_OPTIONS']['additionalBackendItems']['cacheActions'][] =
 		'EXT:enetcache/hooks/class.tx_enetcache_backendContentCacheAction.php:tx_enetcache_backendContentCacheAction';
 
-		// Clear our cache table on "Clear all cache" click and "TCEMAIN.clearCacheCmd = all"
-		// Done by core automatically since 4.6
+	// Clear our cache table on "Clear all cache" click and "TCEMAIN.clearCacheCmd = all"
+	// Done by core automatically since 4.6
 	if (Tx_Enetcache_Utility_Compatibility::convertVersionNumberToInteger(TYPO3_version) <= '4005999') {
 		$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] =
 			'EXT:enetcache/hooks/class.tx_enetcache_backendContentCacheMethods.php:tx_enetcache_backendContentCacheMethods->clearCachePostProc';
 	}
 
-		// Drop cache tag handling in tcemain on changing / inserting / adding records
+	// Drop cache tag handling in tcemain on changing / inserting / adding records
 	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['enetcache'] =
 		'EXT:enetcache/hooks/class.tx_enetcache_tcemain.php:tx_enetcache_tcemain';
 	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['enetcache'] =
 		'EXT:enetcache/hooks/class.tx_enetcache_tcemain.php:tx_enetcache_tcemain';
 
-		// Scheduler task to drop cache entries by tags
-	$TYPO3_CONF_VARS['SC_OPTIONS']['scheduler']['tasks']['tx_enetcache_task_DropTags'] = array(
-		'extension' => $_EXTKEY,
+	// Scheduler task to drop cache entries by tags
+	$TYPO3_CONF_VARS['SC_OPTIONS']['scheduler']['tasks']['tx_enetcache_task_DropTags'] = [
+		'extension' => 'enetcache',
 		'title' => 'LLL:EXT:enetcache/locallang.xml:scheduler.droptags.name',
 		'description' => 'LLL:EXT:enetcache/locallang.xml:scheduler.droptags.description',
 		'additionalFields' => 'tx_enetcache_task_droptags_additionalfieldprovider',
-	);
+    ];
 
-		// CLI script to drop cache entries by tags
-	$TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array(
+	// CLI script to drop cache entries by tags
+	$TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys']['enetcache'] = [
 		'EXT:enetcache/cli/class.tx_enetcache_cli.php', '_CLI_enetcache'
-	);
+	];
 }
-?>
--- ext_tables.php
+++ ext_tables.php
@@ -3,12 +3,11 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-	// Add context sensitive help (csh) to the backend module (used for the scheduler tasks)
-t3lib_extMgm::addLLrefForTCAdescr('_MOD_tools_txschedulerM1', 'EXT:' . $_EXTKEY . '/locallang_csh.xml');
+// Add context sensitive help (csh) to the backend module (used for the scheduler tasks)
+t3lib_extMgm::addLLrefForTCAdescr('_MOD_tools_txschedulerM1', 'EXT:enetcache/locallang_csh.xml');
 
-	// Use sprite icon API for clearContentCache icon in BE cache topMenu, 4.4 and above
-$enetcacheIcons = array(
+// Use sprite icon API for clearContentCache icon in BE cache top Menu
+$enetcacheIcons = [
 	'clearcontentcache' => t3lib_extMgm::extRelPath('enetcache') . 'res/delete_pi.png',
-);
+];
 t3lib_SpriteManager::addSingleIcons($enetcacheIcons, 'enetcache');
-?>
