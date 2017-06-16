2f221883ff7698a536aa3801345db8fc0c358d1f
Christian Kuhn <lolli@schwarzbu.ch>
2017-06-16 23:36:32 +0200

    [!!!] Namespace some classes
    Class renaming:
    * tx_enetcache -> Lolli\Enetcache\FrontendCache
    * tx_enetcache_cachable -> Lolli\Enetcache\FrontendCacheHookInterface
    
    Drop interface tx_enetcache_cachable

---
+++ Classes/FrontendCacheHookInterface.php
@@ -0,0 +1,26 @@
+<?php
+namespace Lolli\Enetcache;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Describes methods hooks must implement if hooking into FrontendCache::class,
+ * see that class for details on what the methods do and provide.
+ */
+interface FrontendCacheHookInterface {
+	public function get($params);
+	public function set(&$params);
+	public function flush();
+	public function drop(&$params);
+}
