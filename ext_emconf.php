6a26f83f2375d156440b302b437ccf98c602e75a
Christian Kuhn <lolli@schwarzbu.ch>
2018-10-27 13:03:51 +0200

    [TASK] drop core v8, add v9 compat

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,8 +13,8 @@ $EM_CONF['enetcache'] = [
     'author_email' => 'lolli@schwarzbu.ch',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
-            'php' => '7.0.0-7.2.99',
+            'typo3' => '9.5.0-9.5.99',
+            'php' => '7.2.0-7.2.99',
         ],
         'conflicts' => [
         ],
