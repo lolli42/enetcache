bd95fbbe0aae613d4ed07603db147789bb10a8c0
Christian Kuhn <lolli@schwarzbu.ch>
2018-10-28 13:59:35 +0100

    [TASK] Remove unused stuff from runTests.sh

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -28,15 +28,12 @@ setUpDockerComposeDotEnv() {
     echo "DOCKER_PHP_IMAGE=${DOCKER_PHP_IMAGE}" >> .env
     echo "EXTRA_TEST_OPTIONS=${EXTRA_TEST_OPTIONS}" >> .env
     echo "SCRIPT_VERBOSE=${SCRIPT_VERBOSE}" >> .env
-    echo "PHPUNIT_RANDOM=${PHPUNIT_RANDOM}" >> .env
-    echo "CGLCHECK_DRY_RUN=${CGLCHECK_DRY_RUN}" >> .env
 }
 
 # Load help text into $HELP
 read -r -d '' HELP <<EOF
-TYPO3 core test runner. Execute acceptance, unit, functional and other test suites in
-a docker based test environment. Handles execution of single test files, sending
-xdebug information to a local IDE and more.
+enetcache test runner. Execute unit test suite and some other details. Alos used
+by travis-ci for test execution.
 
 Successfully tested with docker version 18.06.1-ce and docker-compose 1.21.2.
 
@@ -47,55 +44,25 @@ No arguments: Run all unit tests with PHP 7.2
 Options:
     -s <...>
         Specifies which test suite to run
-            - acceptance: backend acceptance tests
-            - buildCss: execute scss to css builder
-            - buildJavascript: execute typescript to javascript builder
-            - cglGit: test and fix latest committed patch for CGL compliance
-            - cglAll: test and fix all core php files
-            - checkAnnotations: check php code for allowed annotations
-            - checkComposer: check composer.json files for version integrity
-            - checkCsvFixtures: test integrity of functional test csv fixtures
-            - checkExceptionCodes: test core for duplicate exception codes
-            - checkExtensionScannerRst: test all .rst files referenced by extension scanner exist
-            - checkFilePathLength: test core file paths do not exceed maximum length
-            - checkGitSubmodule: test core git has no sub modules defined
-            - checkPermissions: test some core files for correct executable bits
-            - checkRst: test .rst files for integrity
-            - checkXlf: test .xlf files for integrity
             - composerInstall: "composer install", handy if host has no PHP, uses composer cache of users home
             - composerValidate: "composer validate"
-            - functional: functional tests
-            - install: installation acceptance tests, only with -d mariadb|postgres|sqlite
             - lint: PHP linting
-            - lintScss: SCSS linting
-            - lintTypescript: TS linting
             - unit (default): PHP unit tests
-            - unitDeprecated: deprecated PHP unit tests
-            - unitJavascript: JavaScript unit tests
-            - unitRandom: PHP unit tests in random order, add -o <number> to use specific seed
-
-    -d <mariadb|mssql|postgres|sqlite>
-        Only with -s install|functional
-        Specifies on which DBMS tests are performed
-            - mariadb (default): use mariadb
-            - mssql: use mssql microsoft sql server
-            - postgres: use postgres
-            - sqlite: use sqlite
 
     -p <7.2|7.3>
         Specifies the PHP minor version to be used
             - 7.2 (default): use PHP 7.2
             - 7.3: use PHP 7.3
 
-    -e "<phpunit or codeception options>"
-        Only with -s functional|unit|unitDeprecated|unitRandom
-        Additional options to send to phpunit (unit & functional tests) or codeception (acceptance
-        tests). For phpunit, options starting with "--" must be added after options starting with "-".
+    -e "<phpunit options>"
+        Only with -s unit
+        Additional options to send to phpunit tests.
+        For phpunit, options starting with "--" must be added after options starting with "-".
         Example -e "-v --filter canRetrieveValueWithGP" to enable verbose output AND filter tests
         named "canRetrieveValueWithGP"
 
     -x
-        Only with -s functional|unit|unitDeprecated|unitRandom
+        Only with -s unit
         Send information to host instance for test or system under test break points. This is especially
         useful if a local PhpStorm instance is listening on default xdebug port 9000. A different port
         can be selected with -y
@@ -103,16 +70,6 @@ Options:
     -y <port>
         Send xdebug information to a different port than default 9000 if an IDE like PhpStorm
         is not listening on default port.
-
-    -o <number>
-        Only with -s unitRandom
-        Set specific random seed to replay a random run in this order again. The phpunit randomizer
-        outputs the used seed at the end (in bamboo core testing logs, too). Use that number to
-        replay the unit tests in that order.
-
-    -n
-        Only with -s cglGit|cglAll
-        Activate dry-run in CGL check that does not actively change files and only prints broken ones.
 
     -u
         Update existing typo3gmbh/phpXY:latest docker images. Maintenance call to docker pull latest
@@ -127,27 +84,11 @@ Options:
         Show this help.
 
 Examples:
-    # Run all core unit tests using PHP 7.2
+    # Run unit tests using PHP 7.2
     ./Build/Scripts/runTests.sh
-    ./Build/Scripts/runTests.sh -s unit
 
-    # Run all core units tests and enable xdebug (have a PhpStorm listening on port 9000!)
-    ./Build/Scripts/runTests.sh -x
-
-    # Run unit tests in phpunit verbose mode with xdebug on PHP 7.3 and filter for test canRetrieveValueWithGP
-    ./Build/Scripts/runTests.sh -x -p 7.3 -e "-v --filter canRetrieveValueWithGP"
-
-    # Run unit tests with PHP 7.3 and have xdebug enabled
-    ./Build/Scripts/runTests.sh -x -p 7.3
-
-    # Run functional tests on postgres with xdebug, php 7.3 and execute a restricted set of tests
-    ./Build/Scripts/runTests.sh -x -p 7.3 -s functional -d postgres typo3/sysext/core/Tests/Functional/Authentication
-
-    # Run restricted set of backend acceptance tests
-    ./Build/Scripts/runTests.sh -s acceptance typo3/sysext/core/Tests/Acceptance/Backend/Login/BackendLoginCest.php
-
-    # Run installer tests of a new instance on sqlite
-    ./Build/Scripts/runTests.sh -s install -d sqlite
+    # Run unit tests using PHP 7.3
+    ./Build/Scripts/runTests.sh -p 7.3
 EOF
 
 # Test if docker-compose exists, else exit out with error
@@ -173,8 +114,6 @@ PHP_XDEBUG_ON=0
 PHP_XDEBUG_PORT=9000
 EXTRA_TEST_OPTIONS=""
 SCRIPT_VERBOSE=0
-PHPUNIT_RANDOM="--order rand"
-CGLCHECK_DRY_RUN=""
 
 # Option parsing
 # Reset in case getopts has been used previously in the shell
@@ -187,9 +126,6 @@ while getopts ":s:d:p:e:xy:o:nhuv" OPT; do
         s)
             TEST_SUITE=${OPTARG}
             ;;
-        d)
-            DBMS=${OPTARG}
-            ;;
         p)
             PHP_VERSION=${OPTARG}
             ;;
@@ -201,12 +137,6 @@ while getopts ":s:d:p:e:xy:o:nhuv" OPT; do
             ;;
         y)
             PHP_XDEBUG_PORT=${OPTARG}
-            ;;
-        o)
-            PHPUNIT_RANDOM="--order :${OPTARG}"
-            ;;
-        n)
-            CGLCHECK_DRY_RUN="-n"
             ;;
         h)
             echo "${HELP}"
@@ -251,101 +181,6 @@ fi
 
 # Suite execution
 case ${TEST_SUITE} in
-    acceptance)
-        setUpDockerComposeDotEnv
-        docker-compose run prepare_acceptance_backend_mariadb10
-        docker-compose run acceptance_backend_mariadb10
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    buildCss)
-        setUpDockerComposeDotEnv
-        docker-compose run build_css
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    buildJavascript)
-        setUpDockerComposeDotEnv
-        docker-compose run build_javascript
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    cglGit)
-        setUpDockerComposeDotEnv
-        docker-compose run cgl_git
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    cglAll)
-        # Active dry-run for cglAll needs not "-n" but specific options
-        if [[ ! -z ${CGLCHECK_DRY_RUN} ]]; then
-            CGLCHECK_DRY_RUN="--dry-run --diff --diff-format udiff"
-        fi
-        setUpDockerComposeDotEnv
-        docker-compose run cgl_all
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkAnnotations)
-        setUpDockerComposeDotEnv
-        docker-compose run check_annotations
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkComposer)
-        setUpDockerComposeDotEnv
-        docker-compose run check_composer
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkCsvFixtures)
-        setUpDockerComposeDotEnv
-        docker-compose run check_csv_fixtures
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkExceptionCodes)
-        setUpDockerComposeDotEnv
-        docker-compose run check_exception_codes
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkExtensionScannerRst)
-        setUpDockerComposeDotEnv
-        docker-compose run check_extension_scanner_rst
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkFilePathLength)
-        setUpDockerComposeDotEnv
-        docker-compose run check_file_path_length
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkGitSubmodule)
-        setUpDockerComposeDotEnv
-        docker-compose run check_git_submodule
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkPermissions)
-        setUpDockerComposeDotEnv
-        docker-compose run check_permissions
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkRst)
-        setUpDockerComposeDotEnv
-        docker-compose run check_rst
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    checkXlf)
-        setUpDockerComposeDotEnv
-        docker-compose run check_xlf
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
     composerInstall)
         setUpDockerComposeDotEnv
         docker-compose run composer_install
@@ -358,102 +193,15 @@ case ${TEST_SUITE} in
         SUITE_EXIT_CODE=$?
         docker-compose down
         ;;
-    functional)
-        setUpDockerComposeDotEnv
-        case ${DBMS} in
-            mariadb)
-                docker-compose run prepare_functional_mariadb10
-                docker-compose run functional_mariadb10
-                SUITE_EXIT_CODE=$?
-                ;;
-            mssql)
-                docker-compose run prepare_functional_mssql2017cu9
-                docker-compose run functional_mssql2017cu9
-                SUITE_EXIT_CODE=$?
-                ;;
-            postgres)
-                docker-compose run prepare_functional_postgres10
-                docker-compose run functional_postgres10
-                SUITE_EXIT_CODE=$?
-                ;;
-            sqlite)
-                docker-compose run prepare_functional_sqlite
-                docker-compose run functional_sqlite
-                SUITE_EXIT_CODE=$?
-                ;;
-            *)
-                echo "Invalid -d option argument ${DBMS}" >&2
-                echo >&2
-                echo "${HELP}" >&2
-                exit 1
-        esac
-        docker-compose down
-        ;;
-    install)
-        setUpDockerComposeDotEnv
-        case ${DBMS} in
-            mariadb)
-                docker-compose run prepare_acceptance_install_mariadb10
-                docker-compose run acceptance_install_mariadb10
-                SUITE_EXIT_CODE=$?
-                ;;
-            postgres)
-                docker-compose run prepare_acceptance_install_postgres10
-                docker-compose run acceptance_install_postgres10
-                SUITE_EXIT_CODE=$?
-                ;;
-            sqlite)
-                docker-compose run prepare_acceptance_install_sqlite
-                docker-compose run acceptance_install_sqlite
-                SUITE_EXIT_CODE=$?
-                ;;
-            *)
-                echo "Invalid -d option argument ${DBMS}" >&2
-                echo >&2
-                echo "${HELP}" >&2
-                exit 1
-        esac
-        docker-compose down
-        ;;
     lint)
         setUpDockerComposeDotEnv
         docker-compose run lint
         SUITE_EXIT_CODE=$?
         docker-compose down
         ;;
-    lintScss)
-        setUpDockerComposeDotEnv
-        docker-compose run lint_scss
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    lintTypescript)
-        setUpDockerComposeDotEnv
-        docker-compose run lint_typescript
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
     unit)
         setUpDockerComposeDotEnv
         docker-compose run unit
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    unitDeprecated)
-        setUpDockerComposeDotEnv
-        docker-compose run unitDeprecated
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    unitJavascript)
-        setUpDockerComposeDotEnv
-        docker-compose run unitJavascript
-        SUITE_EXIT_CODE=$?
-        docker-compose down
-        ;;
-    unitRandom)
-        setUpDockerComposeDotEnv
-        docker-compose run unitRandom
         SUITE_EXIT_CODE=$?
         docker-compose down
         ;;
