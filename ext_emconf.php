121cb4737819b42b0b6f3e70946014c8c1662e97
Christian Kuhn <lolli@schwarzbu.ch>
2020-11-13 16:19:25 +0100

    [TASK] Declare compat with PHP 7.4 in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF['enetcache'] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
-            'php' => '7.2.0-7.3.99',
+            'php' => '7.2.0-7.4.99',
         ],
         'conflicts' => [
         ],
